5fa495a5f94dcd1d6e044f6aaad54a036fb7ee85
2gn <101851090+2gn@users.noreply.github.com>
2022-08-30 08:11:22 +0900

    overimporting
    removed caches

--- mis.py
+++ mis.py
@@ -1,6 +1,4 @@
-import urllib3
-
-http = urllib3.PoolManager()
+from urllib3 import PoolManager
 
 def _readReturnFile(filename):
     with open(filename, "r") as f:
@@ -8,7 +6,7 @@ def _readReturnFile(filename):
 
 def searchByCallsign(callsign, use_cache=False):
     theUrl = "https://www.tele.soumu.go.jp/musen/SearchServlet?MA={}&SelectID=1&SelectOW=01&DC=100&SK=2&pageID=5&SC=1&CONFIRM=1".format(callsign)
-    data = http.request("GET",theUrl).data.decode("shift-jis") if not use_cache else _readReturnFile("cache")
+    data = PoolManager().request("GET",theUrl).data.decode("shift-jis") if not use_cache else _readReturnFile("cache")
     with open("cache", "w") as f:
         f.write(data)
     data = data.splitlines()
